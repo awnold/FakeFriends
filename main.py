79ec46c42e6f4aa467b1771db13f3cbcf1cdced1
awnold <83228749+awnold@users.noreply.github.com>
2021-12-22 05:41:16 -0500

    Initial Commit
    A working script that generates a text file of the usernames of profiles you follow that do not follow you back.

---
+++ main.py
@@ -0,0 +1,31 @@
+import instaloader
+
+SESSION = instaloader.Instaloader()
+
+USER = "<your bot's username>"
+PASS = "<your bot's password>"
+SESSION.login(USER, PASS)
+
+profile = instaloader.Profile.from_username(SESSION.context, "<username of target>")
+
+follower_list = []
+count = 0
+for follower in profile.get_followers():
+    count += 1
+    print("[" + str(count) + "] " + follower.username)
+    follower_list.append(follower.username)
+
+print("\nFinished Followers! Checking Following...\n")
+count = 0
+
+for followee in profile.get_followees():
+    count += 1
+    followee_username = followee.username
+    if followee_username in follower_list:
+        print("[" + str(count) + "] [YES] " + followee_username)
+    else:
+        print("[" + str(count) + "] [NO] " + followee_username)
+        file = open("FakeFriendsList", "a+")
+        file.write(followee.username + "\n")
+        print
+        file.close()
